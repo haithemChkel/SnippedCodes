fe47049a1622a4aedd9c3ff632b01399c8740d20
KHALIFA Haithem <khalifa.haithem@gmail.com>
2024-01-25 22:22:47 +0100

    qq

--- services_discovery.ps1
+++ services_discovery.ps1
@@ -1,5 +1,5 @@
-param(
-    [string]$processName,
+param (
+    [string[]]$processNames,
     [string]$jsonFilePath
 )
 
@@ -10,74 +10,78 @@ function LogMessage {
     Write-Host "$timestamp - $message"
 }
 
-# Get processes with the specified name
-$matchingProcesses = Get-Process -Name $processName
-
-# Log information about matching processes
-LogMessage "Processes with name '$processName' found:"
-$matchingProcesses | ForEach-Object {
-    LogMessage "Process Name: $($_.ProcessName), PID: $($_.Id)"
-}
-
-# Get the list of process IDs
-$processIds = $matchingProcesses.Id
-
-# Get the list of ports in the "Listen" state used by the specified processes
-$ports = Get-NetTCPConnection | Where-Object { $_.OwningProcess -in $processIds -and $_.State -eq 'Listen' } | Select-Object -ExpandProperty LocalPort
-
-LogMessage "Found ports in 'Listen' state for processes with name '$processName': $ports"
-
 # Initialize an empty array to store job results
 $jobResults = @()
 
-# Run jobs in parallel for each port
-foreach ($port in $ports) {
-    $url = "http://localhost:$port/metrics"
-    LogMessage "Starting job for Port $port"
-    $job = Start-Job -ScriptBlock {
-        param($url, $port)
+foreach ($processName in $processNames) {
+    # Get processes with the specified name
+    $matchingProcesses = Get-Process -Name $processName
 
-        # Function to log messages
-        function LogMessage {
-            param($message)
-            $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-            Write-Host "$timestamp - $message"
-        }
+    # Log information about matching processes
+    LogMessage "Processes with name '$processName' found:"
+    $matchingProcesses | ForEach-Object {
+        LogMessage "Process Name: $($_.ProcessName), PID: $($_.Id)"
+    }
 
-        # Function to check the HTTP status code
-        function CheckHttpStatusCode {
-            param($url, $port)
-            try {
-                $response = Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop
-                $statusCode = $response.StatusCode
-                LogMessage "Port $port - HTTP Status Code: $statusCode"
-                return [PSCustomObject]@{
-                    Port = $port
-                    StatusCode = $statusCode
-                }
-            } catch {
-                $statusCode = $_.Exception.Response.StatusCode.Value__
-                LogMessage "Port $port - Error: $_"
-                return [PSCustomObject]@{
-                    Port = $port
-                    StatusCode = $statusCode
+    # Get the list of process IDs
+    $processIds = $matchingProcesses.Id
+
+    # Get the list of ports in the "Listen" state used by the specified processes
+    $ports = Get-NetTCPConnection | Where-Object { $_.OwningProcess -in $processIds -and $_.State -eq 'Listen' } | Select-Object -ExpandProperty LocalPort
+
+    LogMessage "Found ports in 'Listen' state for processes with name '$processName': $ports"
+
+    # Run jobs in parallel for each port
+    foreach ($port in $ports) {
+        $url = "http://localhost:$port/metrics"
+        LogMessage "Starting job for Process $processName, Port $port"
+        $job = Start-Job -ScriptBlock {
+            param($url, $port, $processName)
+
+            # Function to log messages
+            function LogMessage {
+                param($message)
+                $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                Write-Host "$timestamp - $message"
+            }
+
+            # Function to check the HTTP status code
+            function CheckHttpStatusCode {
+                param($url, $port)
+                try {
+                    $response = Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop
+                    $statusCode = $response.StatusCode
+                    LogMessage "Process $processName, Port $port - HTTP Status Code: $statusCode"
+                    return [PSCustomObject]@{
+                        ProcessName = $processName
+                        Port = $port
+                        StatusCode = $statusCode
+                    }
+                } catch {
+                    $statusCode = $_.Exception.Response.StatusCode.Value__
+                    LogMessage "Process $processName, Port $port - Error: $_"
+                    return [PSCustomObject]@{
+                        ProcessName = $processName
+                        Port = $port
+                        StatusCode = $statusCode
+                    }
                 }
             }
-        }
 
-        # Call the function within the job script block
-        $result = CheckHttpStatusCode -url $url -port $port
+            # Call the function within the job script block
+            $result = CheckHttpStatusCode -url $url -port $port
 
-        # Output the target and labels
-        [PSCustomObject]@{
-            targets = @("localhost:$($result.Port)")
-            labels = @{
-                job = "node"
+            # Output the target and labels
+            [PSCustomObject]@{
+                targets = @("localhost:$($result.Port)")
+                labels = @{
+                    job = $result.ProcessName
+                }
             }
-        }
 
-    } -ArgumentList $url, $port
-    $jobResults += $job
+        } -ArgumentList $url, $port, $processName
+        $jobResults += $job
+    }
 }
 
 # Wait for all jobs to complete
@@ -99,5 +103,5 @@ LogMessage "JSON file created: $jsonFilePath"
 Remove-Job -Job $jobResults
 
 
-# sample
-# .\YourScript.ps1 -processName "Etl.Service.Rafale" -jsonFilePath "result.json"
+# usage
+# .\YourScript.ps1 -processNames @("Etl.Service.Rafale", "AnotherProcess") -jsonFilePath "result.json"
