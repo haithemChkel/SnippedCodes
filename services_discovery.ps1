79d732c3e54ffd339c845390648b82de8d99988c
KHALIFA Haithem <khalifa.haithem@gmail.com>
2024-01-25 17:48:10 +0100

    ff

--- services_discovery.ps1
+++ services_discovery.ps1
@@ -2,33 +2,45 @@
 $processName = "Etl.Service.Rafale"
 
 # Function to check the HTTP status code
-function CheckHttpStatusCode($url) {
+function CheckHttpStatusCode {
+    param($url, $port)
     try {
         $response = Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop
-        return $response.StatusCode
+        $statusCode = $response.StatusCode
+        return [PSCustomObject]@{
+            Port = $port
+            StatusCode = $statusCode
+        }
     } catch {
-        return $_.Exception.Response.StatusCode.Value__
+        return [PSCustomObject]@{
+            Port = $port
+            StatusCode = $_.Exception.Response.StatusCode.Value__
+        }
     }
 }
 
-# Get the list of ports used by processes with the specified name
-$ports = Get-NetTCPConnection | Where-Object { $_.OwningProcess -in (Get-Process -Name $processName).Id } | Select-Object -ExpandProperty LocalPort
+# Get the list of ports in the "Listen" state used by processes with the specified name
+$ports = Get-NetTCPConnection | Where-Object { $_.OwningProcess -in (Get-Process -Name $processName).Id -and $_.State -eq 'Listen' } | Select-Object -ExpandProperty LocalPort
 
-# Initialize an empty list to store ports with HTTP status code 200
-$portsWithStatusCode200 = @()
+# Initialize an empty array to store job results
+$jobResults = @()
 
-# Run parallel checks for each port
-$ports | ForEach-Object -Parallel {
-    param($port)
+# Run jobs in parallel for each port
+foreach ($port in $ports) {
     $url = "http://localhost:$port/metrics"
-    $statusCode = CheckHttpStatusCode $url
+    $job = Start-Job -ScriptBlock {
+        param($url, $port)
+        CheckHttpStatusCode -url $url -port $port
+    } -ArgumentList $url, $port
+    $jobResults += $job
+}
 
-    if ($statusCode -eq 200) {
-        $portsWithStatusCode200 += $port
-    }
+# Wait for all jobs to complete
+Wait-Job -Job $jobResults | Out-Null
 
-} -ArgumentList $_
+# Receive job results
+$receivedJobResults = Receive-Job -Job $jobResults
 
-# Display the list of ports with HTTP status code 200
-Write-Host "Ports with HTTP status code 200 for process '$processName':"
-$portsWithStatusCode200
+# Display the list of ports with HTTP status code 200 in 'Listen' state for the specified process
+Write-Host "Ports with HTTP status code 200 in 'Listen' state for process '$processName':"
+$receivedJobResults | Where-Object { $_.StatusCode -eq 200 } | Select-Object Port
